a4d429806a42401cf913ffd4323d98c1bcbaf112
jorola0411 <jorola@my.bcit.ca>
2024-11-17 21:10:44 -0800

    Home edit and creation of lelem project page

--- portfolio-website/src/components/Home.jsx
+++ portfolio-website/src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import MediaQuery from 'react-responsive';
+import { Tilt } from 'react-tilt';
+import {  Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const tilt = {
+	reverse:        false,  // reverse the tilt direction
+	max:            35,     // max tilt rotation (degrees)
+	perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	scale:          1.2,    // 2 = 200%, 1.5 = 150%, etc..
+	speed:          500,   // Speed of the enter/exit transition
+	transition:     true,   // Set a transition on enter/exit.
+	axis:           'y',   // What axis should be disabled. Can be X or Y.
+	reset:          true,    // If the tilt effect has to be reset on exit.
+	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 function Home(){
 
     return(
-        <div className="min-h-screen flex items-center justify-center ">
+      
+      <MediaQuery minWidth={1024}>
+        <div className="min-h-screen flex flex-col  items-center justify-center ">
         <main className="container mx-auto flex-grow">
         <div className="text-center">
           <h1 className="text-5xl font-semibold text-gray-800 font-karma ">
@@ -15,34 +33,92 @@ function Home(){
 
           
           <div className="mx-auto space-y-2 max-w-screen-lg mt-20 ">
-          <h2 className="font-karma text-3xl font-bold">Featured Works</h2>
+          <h2 className="font-karma text-3xl font-bold text-center">Featured Works</h2>
           <hr className="border-t-2 border-black  mt-2 mb-4" />
           </div>
 
-          <div className="max-w-screen-lg mx-auto grid grid-cols-4 lg:grid-cols-4 gap-8 items-start">
-
+          <div className="max-w-screen-lg mx-auto grid grid-cols-2 lg:grid-cols-4 gap-8 items-center justify-center mt-10">
+          <div className="col-span-1 group flex flex-col items-center">
+            <Tilt options={tilt} style={{ height: 240, width: 190 }}>
+            <Link to ="projects/styles">   
+    <motion.button  initial={{ "--x": "100%", scale: 1 }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.97 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 1,
+        type: "spring",
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className=" rounded-md relative radial-gradient"
+    >
+        <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask ">
+           <img src="../src/assets/stylespolaroid.png"></img>
+            </span>
+            <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+            </motion.button>
+            </Link>
+            </Tilt>
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center mt-10">
+    <h2 className="font-semibold text-lg">Styles, by Jao</h2>
+    <p className="text-sm text-gray-600">UI/UX focused project on a niche app idea.</p>
+              </div>
+                </div>
       
+                <div className="col-span-1 group flex flex-col items-center">
+            <Tilt options={tilt} style={{ height: 240, width: 190 }}>
+            <Link to ="projects/lelem">   
+    <motion.button  initial={{ "--x": "100%", scale: 1 }}
+      animate={{ "--x": "-100%" }}
+      whileTap={{ scale: 0.97 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 1,
+        type: "spring",
+        stiffness: 20,
+        damping: 15,
+        mass: 2,
+        scale: {
+          type: "spring",
+          stiffness: 10,
+          damping: 5,
+          mass: 0.1,
+        },
+      }}
+      className=" rounded-md relative radial-gradient"
+    >
+        <span className="text-neutral-100 tracking-wide font-light h-full w-full block relative linear-mask ">
+           <img src="../src/assets/lelempolaroid.png"></img>
+            </span>
+            <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+            </motion.button>
+            </Link>
+            </Tilt>
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center mt-10">
+    <h2 className="font-semibold text-lg">Lelem Natural Park</h2>
+    <p className="text-sm text-gray-600">A fictional zoo focusing on front-end development.</p>
+              </div>
+                </div>
           
-          <div className="bg-white p-2 shadow-lg rounded-md border border-gray-200 text-center md:col-span-2 ">
-                
-                <p className="text-9xl flex items-center justify-center h-64 bg-light-grey">📝</p> 
-               <p className="mt-2 font-medium font-ubuntu">styles - UI/UX</p>
-             </div>
-          
-          <div className="bg-white p-2 shadow-lg rounded-md border border-gray-200 text-center md:col-span-2 ">
-                
-                <p className="text-9xl flex items-center justify-center h-64 bg-light-grey">📝</p> 
-               <p className="mt-2 font-medium font-ubuntu">styles - UI/UX</p>
-             </div>
 
-          
           </div>
           
         
           
       </main>
     </div>
-        
+    </MediaQuery> 
+
     )
 }
 
--- portfolio-website/src/components/Lelem.jsx
+++ portfolio-website/src/components/Lelem.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-
+import MediaQuery from "react-responsive";
 
 
 
 function Lelem() {
   return (
     <>
-    <p>hi</p>
+    <MediaQuery minWidth={1024}>
+    <div className="items-center justify-center ">
+        <div className="max-w-screen-lg mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
+              
+        <div className="md:col-span-3 space-y-2">
+        <h1 className="font-karma text-3xl font-bold">Lelem Natural Park</h1>
+            <hr className="border-t-2 border-black  mt-2 mb-4" />
+            <p>Lelem Natural Park was a group project in my Project Management class during my time in New Media and Web Development at BCIT. It focused on the Agile metholodgy as well as working with group members in a fictional client setting. </p>
+        <h1 className="font-karma text-3xl font-bold">Role & Team</h1>
+            <hr className="border-t-2 border-black  mt-2 mb-4" />
+            <p>For this project, I and 1 other person were the front-end developers of the project, in conjuction with 2 visual designers and 1 copywriter.</p>
+        <h1 className="font-karma text-3xl font-bold">Project Goals</h1>
+            <hr className="border-t-2 border-black  mt-2 mb-4" />
+            <p>The goal of the project was to create a website for a fictional organization for a client, and to showcase proper usage of the Agile project management methodology. </p>
+            </div>
+            </div>
+            </div>
+            </MediaQuery>
     </>
   )
 }

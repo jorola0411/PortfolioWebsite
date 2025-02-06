d49153b1dde5d9adfe8c49e354ba8f2e49c75342
jorola0411 <jorola@my.bcit.ca>
2025-02-05 21:59:01 -0800

    changed link for button in mh page

--- portfolio-website/src/components/Monster.jsx
+++ portfolio-website/src/components/Monster.jsx
@@ -325,7 +325,7 @@ function Home() {
                 <hr className="max-w-[80%] border-t-2 border-black mt-2 mb-4 mx-auto" />
                 <div className='flex gap-4 justify-center mb-3'>
 
-                    <button className="sm:text-sm md:text-lg  bg-beige-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition duration-200 gap-10"> <a href="https://www.figma.com/design/c1Wm6EOGfye2l1I9eW3v6X/Clothing-store-WireFrame?node-id=435-5900&t=GSqSsiB1dNSTdMgr-1" target="blank">
+                    <button className="sm:text-sm md:text-lg  bg-beige-500 text-white px-6 py-2 rounded-full hover:bg-orange-600 transition duration-200 gap-10"> <a href="https://github.com/jorola0411/MonsterHunter-API-project.git" target="blank">
                         GitHub
                     </a></button>
 
